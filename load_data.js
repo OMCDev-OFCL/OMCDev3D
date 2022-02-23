fd071bafd042331af7b4191fcb9909af61c36a3d
OMCDev <73343617+OMCDev-OFCL@users.noreply.github.com>
2022-02-23 14:02:11 +0530

    Add files via upload

---
+++ load_data.js
@@ -0,0 +1,60 @@
+var firebaseConfig = {    
+    databaseURL: "https://omcdev-10317.firebaseio.com"
+};
+firebase.initializeApp(firebaseConfig);
+function inst(unity_inst)
+{    
+    firebase.database().ref('/Projects').once('value',function(snapshot)
+    {
+        snapshot.forEach(function(childSnapshot)
+        {
+            var childkey = childSnapshot.key;
+            var childdata = childSnapshot.val();
+            let new_item =
+            {
+                'key' : childkey,
+                'data' : childdata
+            }
+            unity_inst.SendMessage('data_loader','set_ptitle',new_item.data['ptitle'].toString());
+            unity_inst.SendMessage('data_loader','set_psno',new_item.data['psno'].toString());
+            unity_inst.SendMessage('data_loader','set_pshortds',new_item.data['pshortds'].toString());
+            unity_inst.SendMessage('data_loader','set_plstore',new_item.data['plstore'].toString());            
+            unity_inst.SendMessage('data_loader','set_plongds',new_item.data['plongds'].toString());
+            unity_inst.SendMessage('data_loader','set_picolink',new_item.data['picolink'].toString());
+            unity_inst.SendMessage('data_loader','set_pbnrlink',new_item.data['pbnrlink'].toString());
+            unity_inst.SendMessage('data_loader','set_Previewlink',new_item.data['Previewlink'].toString());
+            unity_inst.SendMessage('data_loader','add_temp');
+        });
+        firebase.database().ref('/contributors').once('value',function(snapshot){
+            snapshot.forEach(function(childSnapshot){
+                var childkey = childSnapshot.key;
+                var childdata = childSnapshot.val();
+                let new_item =
+                {
+                    'key' : childkey,
+                    'data' : childdata
+                }
+                unity_inst.SendMessage('data_loader','set_ctitle',new_item.data['ctitle'].toString());
+                console.log("person : " + new_item.data['ctitle'].toString());
+                unity_inst.SendMessage('data_loader','set_cno',new_item.data['cno'].toString());
+                unity_inst.SendMessage('data_loader','set_cshortds',new_item.data['cshortds'].toString());
+                unity_inst.SendMessage('data_loader','set_cicolink',new_item.data['cicolink'].toString());
+                unity_inst.SendMessage('data_loader','set_clink',new_item.data['clink'].toString());
+                unity_inst.SendMessage('data_loader','add_temp_contrib');
+            });
+            unity_inst.SendMessage('data_loader','generat_platform');     
+        });              
+    });    
+}
+function set_view()
+{
+    var device_width = window.innerWidth;
+    var device_height = window.innerHeight;
+    var container = document.getElementById("unity-container").style;
+    if(device_height < device_width)
+    {
+        container.width = device_width+'px';
+        container.height = device_height+'px';
+    }
+}
+set_view();
